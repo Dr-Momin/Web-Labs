401be0dc83acaf2208612e323539fe42997c1ca4
Momin Khan <mmomin.bscsf19@iba-suk.edu.pk>
2022-05-01 16:18:02 +0500

    updated...

---
+++ Web_Labs/Lab05/TicTacToe/script.js
@@ -0,0 +1,7 @@
+console.log("Welcome to Tic Tac Toe")
+let music = new Audio("./res/music.mp3")
+let audioTurn = new Audio("./res/ting.mp3")
+let gameover = new Audio("./res/gameover.mp3")
+let turn = "X"
+let isgameover = false;
+
